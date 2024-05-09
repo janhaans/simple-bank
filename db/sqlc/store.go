60169a3916608b8e9e77c116a36191961d763c3d
Jan Haans <jan.haans@gmail.com>
2024-05-09 16:02:02 +0200

    Added Tranfer Transaction

---
+++ db/sqlc/store.go
@@ -0,0 +1,93 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+)
+
+// provide all functions to execute DB queries and transactions
+type Store struct {
+	*Queries
+	db *sql.DB
+}
+
+// NewStore creates a new store
+func NewStore(db *sql.DB) *Store {
+	return &Store{
+		db:      db,
+		Queries: New(db),
+	}
+}
+
+// execTX executes a function within a DB transaction
+func (store *Store) execTX(ctx context.Context, fn func(q *Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			return fmt.Errorf("tx err = %v, rb err = %v", err, rbErr)
+		}
+		return err
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxtParams struct {
+	FromAccountID int64 `json:"from_account_id"`
+	ToAccountID   int64 `json:"to_account_id"`
+	Amount        int64 `json:"amount"`
+}
+
+type TransferTxtResult struct {
+	Transfer    Transfer `json:"transfer"`
+	FromEntry   Entry    `json:"from_entry"`
+	ToEntry     Entry    `json:"to_entry"`
+	FromAccount Account  `json:"from_account"`
+	ToAccount   Account  `json:"to_account"`
+}
+
+func (store *Store) TransferTxt(ctx context.Context, arg TransferTxtParams) (TransferTxtResult, error) {
+	var result TransferTxtResult
+
+	err := store.execTX(ctx, func(q *Queries) error {
+		var err error
+
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			FromAccountID: arg.FromAccountID,
+			ToAccountID:   arg.ToAccountID,
+			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		//TODO: update account's balance
+
+		return nil
+	})
+
+	return result, err
+}
